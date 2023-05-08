aad45524dde0b4d562e7b69498ea4b58d850693b
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-05-08 00:21:38 -0400

    Update buildme.sh

--- buildme.sh
+++ buildme.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
-
+echo "Starting... $(date +%Y%m%d%H%M%S)"
 docker run -v $PWD:/volume --rm -t clux/muslrust:stable cargo build --release
 mv target/x86_64-unknown-linux-musl/release/ ./larksys
 echo "Statically linked musl libc binary ./larksys compiled."
 echo
 echo "Running tests..."
 cargo test
+echo "Tests ended, gathering stats and checksums..."
+echo
+which tokei || cargo install tokei
+tokei | tee workspace_larksys_files.txt
+b2sum ./larksys > larksys_checksums.txt
+sha256sum ./larksys >> larksys_checksums.txt
+sha1sum ./larksys >> larksys_checksums.txt
+md5sum ./larksys >> larksys_checksums.txt
+echo "...Finished $(date +%Y%m%d%H%M%S)"
