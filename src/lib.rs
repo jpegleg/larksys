a0559cd7a3debb593255169c4c569f1d9507cdca
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2023-05-08 00:08:03 -0400

    Create lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn placeholder () {
+      let sordu = "Execute each function and check that it did what we expect.";
+      assert_eq!(sordu,"Execute each function and check that it did what we expect.");
+    }
+}
